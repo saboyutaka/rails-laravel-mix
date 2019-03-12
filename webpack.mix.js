24e92691fce60ce2dc5bae5bbb77f79fac476991
saboyutaka <yutaka.paradigm.shift@gmail.com>
2019-03-12 18:44:37 +0900

    Bump laravel-mix into ver 4.0

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,21 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
 
 mix.setPublicPath('public')
-.js('resources/assets/js/app.js', 'public/js')
-.sass('resources/assets/sass/app.scss', 'public/css')
-.extract([
-  'axios',
-  'vue',
-  'jquery',
-  'bootstrap'
-]);
+  .js('resources/js/app.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css');
 
+// productionの時はversioningする
 if (mix.inProduction()) {
   mix.version();
 }
