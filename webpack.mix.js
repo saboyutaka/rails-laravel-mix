26c1fbfd5b5b1f7e4581107d8e7c86ff5d91d7bc
saboyutaka <yutaka.paradigm.shift@gmail.com>
2019-03-12 19:38:49 +0900

    versionやめる

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,3 @@ const mix = require('laravel-mix');
 mix.setPublicPath('public')
   .js('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css');
-
-// productionの時はversioningする
-if (mix.inProduction()) {
-  mix.version();
-}
